5c976da5e4c63824d283dea500979e54109f1d43
Yuri <korytko_88@mail.ru>
2021-12-09 15:06:37 +0300

    HW_Yuri3_JS

---
+++ script.js
@@ -0,0 +1,74 @@
+//задание 1
+
+/*for(i = 1; i <= 50; i++){
+    console.log(i)
+}
+
+for(i = 35; i >= 8; i--){
+    console.log(i)
+}*/
+
+//задание 2
+
+/*let i = 89;
+while( i >= 11){
+    console.log(i);
+    i--;
+}*/
+
+//задание 3
+
+/*let a = 100;
+sum = 0;
+for(let i = 0; i <= a; i++){
+    console.log(i)
+    sum += i;
+}
+console.log(sum);*/
+
+
+//задание 4
+ 
+/*let a = 5;
+let sum = 0;
+for(let i = 1; i <= a; i++){
+    let sum = 0;
+
+    for(let j = 1; j <= i; j++){
+        sum += j;
+    }
+    console.log(`sum` + i + `=` + sum);
+}*/
+
+
+//задание 5
+
+/*for(let i = 8; i <= 56; i++){
+    if(i % 2 == 0)
+    console.log(i)
+}
+
+let i = 8;
+while(i <= 56){
+    //if(i % 2 == 0);
+    console.log(i);
+    i = i + 2;
+}*/
+
+
+//задание 6
+
+/*for(let i = 1; i <= 9; i++){
+
+for(k = 1; k <= 9; k++){
+    
+    document.write(`${i}*${k} = ${k * i} <br>`);
+}
+document.write(`<hr>`);
+   
+}*/
+
+
+
+
+
